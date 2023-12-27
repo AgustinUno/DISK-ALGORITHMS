7641de3db16b3d1fce693db0bc024e12550ebd25
Justine <118758009+AgustinUno@users.noreply.github.com>
2023-12-28 03:27:13 +0800

    SEMI LINEG

--- Practice JS/scripts.js
+++ Practice JS/scripts.js
@@ -6,7 +6,7 @@ var seek_rate = []
 var requested_tracks = []
 var checkRt
 
-function fetchTracks (inputId, arrayName) {
+function fetchTracks(inputId, arrayName) {
   var inputElement = document.getElementById(inputId)
   var inputString = inputElement.value
 
@@ -22,7 +22,7 @@ function fetchTracks (inputId, arrayName) {
   window[arrayName] = tracksArray
 }
 
-function getData () {
+function getData() {
   fetchTracks('nTracks-input', 'no_tracks')
   fetchTracks('prevTracks-input', 'previous_track')
   fetchTracks('currTracks-input', 'current_track')
@@ -37,7 +37,7 @@ function getData () {
   sort()
 }
 
-function sort () {
+function sort() {
   // Sort the array in ascending order
   requested_tracks.sort(function (a, b) {
     return a - b
@@ -48,7 +48,7 @@ function sort () {
   data_Out()
 }
 var thm
-function data_Out () {
+function data_Out() {
   var out = document.getElementById('dataOut')
   out.innerHTML = ''
 
@@ -61,15 +61,14 @@ function data_Out () {
       requested_tracks[requested_tracks.length - 1] -
       requested_tracks[0]
 
-    out.innerHTML += `(${current_track[0]} - ${requested_tracks[0]}) + (${
-      requested_tracks[requested_tracks.length - 1]
-    } - ${requested_tracks[0]})`
+    out.innerHTML += `(${current_track[0]} - ${requested_tracks[0]}) + (${requested_tracks[requested_tracks.length - 1]
+      } - ${requested_tracks[0]})`
 
     console.log(
       'THM = ' +
-        (current_track[0] -
-          requested_tracks[0] +
-          (requested_tracks[requested_tracks.length - 1] - requested_tracks[0]))
+      (current_track[0] -
+        requested_tracks[0] +
+        (requested_tracks[requested_tracks.length - 1] - requested_tracks[0]))
     )
   } else {
     thm =
@@ -77,25 +76,53 @@ function data_Out () {
       current_track[0] +
       (requested_tracks[requested_tracks.length - 1] - requested_tracks[0])
 
-    out.innerHTML += `(${requested_tracks[requested_tracks.length - 1]} - ${
-      current_track[0]
-    }) + (${requested_tracks[requested_tracks.length - 1]} - ${
-      requested_tracks[0]
-    })`
+    out.innerHTML += `(${requested_tracks[requested_tracks.length - 1]} - ${current_track[0]
+      }) + (${requested_tracks[requested_tracks.length - 1]} - ${requested_tracks[0]
+      })`
 
     //  let r = 42;
     //  out.innerHTML = `value: ${r}`;
   }
 
-  out.innerHTML += `<br><b><span style="color: #238636;">THM = ${thm}</span></b>`;
+  out.innerHTML += `<br><b><span style="color: #238636;">THM = ${thm}</span></b>`
 
   //ST
-  var st;
-  var st_ms;
-  st = thm * seek_rate[0];
-  st_ms = st / 100;
+  var st
+  var st_ms
+  st = thm * seek_rate[0]
+  st_ms = st / 100
 
-  console.log("ST: " + st + " or " + st_ms +" s");
+  console.log('ST: ' + st + ' or ' + st_ms + ' s')
 
-  out.innerHTML += `<br><br>ST = ${thm} x ${seek_rate[0]}<br>ST = ${st} / 100<br><b><span style="color: #238636;">ST = ${st_ms} s</span></b>`;
+  out.innerHTML += `<br><br>ST = ${thm} x ${seek_rate[0]}<br>ST = ${st} / 100<br><b><span style="color: #238636;">ST = ${st_ms} s</span></b>`
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+ 
+  var labels = ['Point 1', 'Point 2', 'Point 3']; // Provide meaningful labels
+  var data = {
+      labels: labels,
+      datasets: [{
+          label: 'Monthly Data',
+          data: requested_tracks,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0
+      }]
+  };
+
+  var ctx = document.getElementById('myLineChart').getContext('2d');
+  var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: {
+          indexAxis: 'y',
+          scales: {
+              x: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+});
